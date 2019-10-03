6cec32d58fe6fb1199ce86d5653fe2e483987507
Bailey Wood <woodbmae@gmail.com>
2019-10-03 16:32:04 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,10 +7,10 @@ def run_guessing_game
   input2 = input.to_i
   if input == "exit"
     puts "Goodbye!"
-  elsif input2 =! num
-    puts "Sorry! The computer guessed #{num}."
   elsif input2 == num
     puts "You guessed the correct number!"
+  else
+    puts "Sorry! The computer guessed #{num}."
   end
 end 
 
