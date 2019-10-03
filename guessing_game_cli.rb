90ce41448c6c084c41884bdc925ff7914e520bdc
Bailey Wood <woodbmae@gmail.com>
2019-10-03 16:28:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,6 +4,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6.\n"
   num = rand(1...6)
   input = gets.chomp
+  input2 = input.to_i
   if input == "exit"
     puts "Goodbye!"
   elsif input =! num
